fd0988f4fc615fc0552a43b8f1115d47a03403cf
nana0sei <frimpongnanao@gmail.com>
2023-10-02 14:29:58 +0000

    set up grid areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <>Spotify Lite</>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"main"
+          "controls"   
+          "mobile-nav" `,
+
+          lg: `"nav main"
+      "controls controls"
+      `,
+        }}
+      >
+        <Show above="lg">
+          <GridItem area="nav" bg="orange.300">
+            NavBar
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="yellow.300">
+          Main
+        </GridItem>
+
+        <GridItem area="controls" bg="blue.300">
+          Controls
+        </GridItem>
+
+        <Show below="lg">
+          <GridItem area="mobile-nav" bg="green.300">
+            Mobile Nav
+          </GridItem>
+        </Show>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
