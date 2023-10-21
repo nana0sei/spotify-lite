9dea974eacbe8471801a8f3587f544ef0e76d159
nana0sei <frimpongnanao@gmail.com>
2023-10-21 11:07:34 +0000

    api client setup

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: " https://api.spotify.com",
+  baseURL: " https://api.spotify.com/v1",
   params: {
     clientID: "4c6ae60992184ac7bc189106d32e1074",
     clientSecret: "abe96e51897b427099725255caff0598",
@@ -12,7 +12,7 @@ const axiosInstance = axios.create({
   data: "grant_type=client_credentials&client_id=clientID&client_secret=clientSecret",
 });
 
-class APIClient {
+class APIClient<T> {
   endpoint: string;
 
   constructor(endpoint: string) {
@@ -20,6 +20,12 @@ class APIClient {
   }
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance.get(this.endpoint, config).then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
   };
 }
 
