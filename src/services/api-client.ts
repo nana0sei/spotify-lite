44b171743e50a78e8a0031d61bfa676118e725f7
nana0sei <frimpongnanao@gmail.com>
2023-10-22 16:38:52 +0000

    client update

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -6,16 +6,24 @@ interface FetchResponse<T> {
   results: T[];
 }
 
-const axiosInstance = axios.create({
-  baseURL: " https://api.spotify.com/v1",
-  params: {
-    clientID: "4c6ae60992184ac7bc189106d32e1074",
-    clientSecret: "abe96e51897b427099725255caff0598",
-  },
+const clientID = "4c6ae60992184ac7bc189106d32e1074";
+const clientSecret = "abe96e51897b427099725255caff0598";
+const token = btoa(clientID + ":" + clientSecret);
+
+const tokenInstance = axios.create({
+  baseURL: "https://accounts.spotify.com/api/token",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
   },
-  data: "grant_type=client_credentials&client_id=clientID&client_secret=clientSecret",
+  data: "grant_type=client_credentials&client_id=4c6ae60992184ac7bc189106d32e1074&client_secret=abe96e51897b427099725255caff0598",
+});
+
+const axiosInstance = axios.create({
+  baseURL: " https://api.spotify.com/v1",
+  headers: {
+    Authorization: `Basic ${token}`,
+  },
+  data: "grant_type=client_credentials",
 });
 
 class APIClient<T> {
@@ -24,6 +32,11 @@ class APIClient<T> {
   constructor(endpoint: string) {
     this.endpoint = endpoint;
   }
+
+  getToken = (config: AxiosRequestConfig) => {
+    return tokenInstance.post<T>(this.endpoint, config);
+  };
+
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
       .get<FetchResponse<T>>(this.endpoint, config)
@@ -32,7 +45,9 @@ class APIClient<T> {
 
   get = (id: string) => {
     return axiosInstance
-      .get<T>(this.endpoint + "/" + id)
+      .get<T>(this.endpoint + "/" + id, {
+        headers: { Authorization: `Basic ${token}` },
+      })
       .then((res) => res.data);
   };
 }
