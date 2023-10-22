66ba7485549a1bb7e32a1cbfe694a0032469a2d9
nana0sei <frimpongnanao@gmail.com>
2023-10-22 13:45:39 +0000

    error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,47 @@
+import Navbar from "../components/Navbar";
+import { Grid, Show, GridItem } from "@chakra-ui/react";
+import ErrorMessage from "../components/ErrorMessage";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"main"
+      "controls"   
+      "mobile-nav" `,
+
+          lg: `"nav main"
+  "controls controls"
+  `,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "350px 1fr",
+        }}
+      >
+        <Show above="lg">
+          <GridItem area="nav" padding={2}>
+            <Navbar />
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="yellow.300">
+          <ErrorMessage />
+        </GridItem>
+
+        <GridItem area="controls" bg="blue.300">
+          Controls
+        </GridItem>
+
+        <Show below="lg">
+          <GridItem area="mobile-nav" bg="green.300">
+            Mobile Nav
+          </GridItem>
+        </Show>
+      </Grid>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import Layout from "./pages/Layout";
 import Homepage from "./pages/Homepage";
 import SearchPage from "./pages/SearchPage";
 import AlbumDetailPage from "./pages/AlbumDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Homepage /> },
       { path: "search", element: <SearchPage /> },
