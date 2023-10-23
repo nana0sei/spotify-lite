5980866f7a279cbf80803ce482ea6edfd034ce99
nana0sei <frimpongnanao@gmail.com>
2023-10-23 20:14:24 +0000

    new dependencies

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -5,37 +5,34 @@ interface FetchResponse<T> {
   next: string | null;
   results: T[];
 }
-
+interface Token {
+  access_token: string;
+}
 const clientID = "4c6ae60992184ac7bc189106d32e1074";
 const clientSecret = "abe96e51897b427099725255caff0598";
 const token = btoa(clientID + ":" + clientSecret);
 
-const getAuth = async () => {
-  try {
-    const token_url = "https://accounts.spotify.com/api/token";
-
-    const response = await axios.post(token_url, {
+const getToken = () => {
+  const tokenURL = "https://accounts.spotify.com/api/token";
+  axios
+    .post<Token>(tokenURL, {
       headers: {
         Authorization: "Basic " + token,
         "Content-Type": "application/x-www-form-urlencoded",
       },
+
       data: "grant_type=client_credentials",
-    });
-    return response.data.access_token;
-  } catch (error) {
-    console.log(error);
-  }
+    })
+    .then((res) => res.data.access_token);
 };
 
-const access_token = await getAuth();
-console.log(access_token);
+const access_token = getToken();
 
 const axiosInstance = axios.create({
   baseURL: " https://api.spotify.com/v1",
   headers: {
     Authorization: "Bearer " + access_token,
   },
-  data: "grant_type=client_credentials",
 });
 
 class APIClient<T> {
