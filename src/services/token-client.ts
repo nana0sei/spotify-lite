ea5aa17adf37a0b9d04e33e1c1e956e8cf11463b
nana0sei <frimpongnanao@gmail.com>
2023-10-24 22:06:24 +0000

    token client cleanup

--- src/services/token-client.ts
+++ src/services/token-client.ts
@@ -10,22 +10,22 @@ const clientSecret = "abe96e51897b427099725255caff0598";
 const token = btoa(clientID + ":" + clientSecret);
 const data = qs.stringify({ grant_type: "client_credentials" });
 
-const getAccessToken = async () => {
+const getAccessToken = () => {
   const tokenURL = "https://accounts.spotify.com/api/token";
 
-  try {
-    const response = await axios.post<Token>(tokenURL, data, {
+  return axios
+    .post<Token>(tokenURL, data, {
       headers: {
         Authorization: "Basic " + token,
         "Content-Type": "application/x-www-form-urlencoded",
       },
+    })
+    .then((res) => res.data.access_token)
+    .catch((error) => {
+      // Handle any errors here
+      console.error("Error getting access token:", error);
+      throw error;
     });
-
-    return response.data.access_token;
-  } catch (error) {
-    console.error("Error getting access token:", error);
-    throw error;
-  }
 };
 
 export default getAccessToken;
