cd3506de86c3e6efc62d3b1d74486cd0e2762cd7
nana0sei <frimpongnanao@gmail.com>
2023-10-24 20:26:59 +0000

    getAccessToken

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -14,7 +14,7 @@ const clientSecret = "abe96e51897b427099725255caff0598";
 const token = btoa(clientID + ":" + clientSecret);
 const data = qs.stringify({ grant_type: "client_credentials" });
 
-const getToken = () => {
+const getAccessToken = () => {
   const tokenURL = "https://accounts.spotify.com/api/token";
   axios
     .post<Token>(tokenURL, data, {
@@ -26,7 +26,7 @@ const getToken = () => {
     .then((res) => res.data.access_token);
 };
 
-const access_token = getToken();
+const access_token = getAccessToken();
 
 const axiosInstance = axios.create({
   baseURL: " https://api.spotify.com/v1",
