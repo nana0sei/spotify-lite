2ff9f42a72e52eacba441b325d761c2cd64aeebf
nana0sei <frimpongnanao@gmail.com>
2023-10-26 22:55:19 +0000

    tracklist key

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -11,7 +11,7 @@ const TrackList = () => {
   return (
     <>
       {album.tracks.items.map((tracks, index) => (
-        <HStack spacing={5}>
+        <HStack spacing={5} key={index}>
           <Text key={index} size="2xl">
             {tracks.track_number}
           </Text>
