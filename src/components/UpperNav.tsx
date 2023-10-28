4d31d417dc3056c50c1eeb0fee2dcdd4e65ba026
nana0sei <frimpongnanao@gmail.com>
2023-10-28 11:58:03 +0000

    nav routing

--- src/components/UpperNav.tsx
+++ src/components/UpperNav.tsx
@@ -1,21 +1,26 @@
 import { Card, CardBody, HStack, Heading } from "@chakra-ui/react";
 import { BiSearch } from "react-icons/bi";
 import { AiFillHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const UpperNav = () => {
   return (
     <>
       <Card width="340px">
         <CardBody>
-          <HStack>
-            <AiFillHome size="20px" />
-            <Heading fontSize="lg">Home</Heading>
-          </HStack>
+          <Link to="/">
+            <HStack>
+              <AiFillHome size="20px" />
+              <Heading fontSize="lg">Home</Heading>
+            </HStack>
+          </Link>
 
-          <HStack paddingTop={5}>
-            <BiSearch size="20px" />
-            <Heading fontSize="lg">Search</Heading>
-          </HStack>
+          <Link to="/search">
+            <HStack paddingTop={5}>
+              <BiSearch size="20px" />
+              <Heading fontSize="lg">Search</Heading>
+            </HStack>
+          </Link>
         </CardBody>
       </Card>
     </>
