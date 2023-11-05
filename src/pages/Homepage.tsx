4482f46cfe9b6b746b55bd21eeaf886b9d97b7fd
nana0sei <frimpongnanao@gmail.com>
2023-11-05 10:24:28 +0000

    homepage greeting

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,21 @@
 import { Heading } from "@chakra-ui/react";
 
 const Homepage = () => {
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  let greeting = "Hello";
+
+  if (currentHour >= 5 && currentHour < 12) {
+    greeting = "Good morning";
+  } else if (currentHour >= 12 && currentHour < 18) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good evening";
+  }
+
   return (
     <>
-      <Heading>Homepage</Heading>
+      <Heading fontSize="3xl">{greeting}</Heading>
     </>
   );
 };
