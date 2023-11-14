01a8adaca5edefabf6218b02c826705cf8725342
nana0sei <frimpongnanao@gmail.com>
2023-11-14 14:07:51 +0000

    routing mod

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import SearchPage from "./pages/SearchPage";
 import AlbumDetailPage from "./pages/AlbumDetailPage";
 import ErrorPage from "./pages/ErrorPage";
+import SearchResults from "./components/SearchResults";
 
 const router = createBrowserRouter([
   {
@@ -12,7 +13,11 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Homepage /> },
-      { path: "search", element: <SearchPage /> },
+      {
+        path: "search/",
+        element: <SearchPage />,
+        children: [{ path: ":q", element: <SearchResults /> }],
+      },
       { path: "albums/:id", element: <AlbumDetailPage /> },
     ],
   },
