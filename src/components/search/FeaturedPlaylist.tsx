9ef658b20f4c7ee28b45048eb334b8e7fd99da4c
nana0sei <frimpongnanao@gmail.com>
2023-11-19 17:01:01 +0000

    playlist link

--- src/components/search/FeaturedPlaylist.tsx
+++ src/components/search/FeaturedPlaylist.tsx
@@ -1,6 +1,7 @@
 import useFeaturedPlaylists from "../../hooks/useFeaturedPlaylists";
 import { Card, Image, Text, VStack, SimpleGrid } from "@chakra-ui/react";
 import Summary from "../Summary";
+import { Link } from "react-router-dom";
 
 const FeaturedPlaylist = () => {
   const { data: featured, isLoading, error } = useFeaturedPlaylists();
@@ -11,18 +12,21 @@ const FeaturedPlaylist = () => {
     <>
       <SimpleGrid columns={{ sm: 2, md: 3, lg: 5 }} spacing={5} py={2}>
         {featured.playlists.items.map((playlist, index) => (
-          <Card key={index} bg="gray.800">
-            <VStack p={3} align="flex-start">
-              <Image src={playlist.images[0].url} key={index} />
-              <Text as="b">{playlist.name}</Text>
-              <Summary
-                children={playlist.description}
-                limit={40}
-                color="gray.600"
-                fontSize="sm"
-              />
-            </VStack>
-          </Card>
+          <Link to={`/playlists/${playlist.id}`}>
+            <Card key={index} bg="gray.800" _hover={{ bg: "gray.500" }}>
+              <VStack p={3} align="flex-start">
+                <Image src={playlist.images[0].url} key={index} />
+                <Text as="b">{playlist.name}</Text>
+
+                <Summary
+                  children={playlist.description}
+                  limit={40}
+                  color="gray.600"
+                  fontSize="sm"
+                />
+              </VStack>
+            </Card>
+          </Link>
         ))}
       </SimpleGrid>
     </>
