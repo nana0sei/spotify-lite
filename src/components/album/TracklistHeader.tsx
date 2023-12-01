2257cb8f8e41e7a7b99b62aa89dd1bb9b1d48e50
nana0sei <frimpongnanao@gmail.com>
2023-12-01 17:52:54 +0000

    headers mobile optimization

--- src/components/album/TracklistHeader.tsx
+++ src/components/album/TracklistHeader.tsx
@@ -1,18 +1,20 @@
-import { HStack, Divider, Box, Text } from "@chakra-ui/react";
+import { HStack, Divider, Box, Text, Show } from "@chakra-ui/react";
 import { LuClock3 } from "react-icons/lu";
 
 const TracklistHeader = () => {
   return (
     <>
-      <HStack paddingBottom={2} justifyContent="space-between">
-        <Box>
-          <HStack spacing={5}>
-            <Text>#</Text>
-            <Text>Title</Text>
-          </HStack>
-        </Box>
-        <LuClock3 />
-      </HStack>
+      <Show above="sm">
+        <HStack paddingBottom={2} justifyContent="space-between">
+          <Box>
+            <HStack spacing={5}>
+              <Text>#</Text>
+              <Text>Title</Text>
+            </HStack>
+          </Box>
+          <LuClock3 />
+        </HStack>
+      </Show>
       <Divider />
     </>
   );
--- src/components/playlist/PlaylistTracksHeader.tsx
+++ src/components/playlist/PlaylistTracksHeader.tsx
@@ -4,27 +4,28 @@ import { LuClock3 } from "react-icons/lu";
 const PlaylistTracksHeader = () => {
   return (
     <>
-      <HStack
-        paddingBottom={2}
-        justifyContent="space-between"
-        fontSize="sm"
-        px={1}
-      >
-        <Box>
-          <HStack spacing={5}>
-            <Text>#</Text>
-            <Text>Title</Text>
+      <Show above="sm">
+        <HStack
+          paddingBottom={2}
+          justifyContent="space-between"
+          fontSize="sm"
+          px={1}
+        >
+          <Box>
+            <HStack spacing={5}>
+              <Text>#</Text>
+              <Text>Title</Text>
+            </HStack>
+          </Box>
+          <HStack w="60%" justifyContent="space-between">
+            <Show above="sm">
+              <Text w="30%">Album</Text>
+              <Text>Date Added</Text>
+            </Show>
+            <LuClock3 />
           </HStack>
-        </Box>
-
-        <HStack w="60%" justifyContent="space-between">
-          <Show above="sm">
-            <Text w="30%">Album</Text>
-            <Text>Date Added</Text>
-          </Show>
-          <LuClock3 />
         </HStack>
-      </HStack>
+      </Show>
       <Divider />
     </>
   );
