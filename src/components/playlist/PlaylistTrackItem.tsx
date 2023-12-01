6ed818f9029280e707164caae939dfd1eaf16707
nana0sei <frimpongnanao@gmail.com>
2023-12-01 09:12:43 +0000

    play songs from playlists

---
+++ src/components/playlist/PlaylistTrackItem.tsx
@@ -0,0 +1,107 @@
+import { HStack, VStack, Show, Box, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import ExplicitTag from "../ExplicitTag";
+import Summary from "../Summary";
+import usePlaybackQueryStore from "../../queries/playback-store";
+
+interface Props {
+  id: string;
+  name: string;
+  track_number: number;
+  artists: string[];
+  added_at: string;
+  duration: string;
+  explicit: boolean;
+  album: {
+    link: string;
+    name: string;
+  };
+}
+
+const PlaylistTrackItem = ({
+  id,
+  name,
+  track_number,
+  artists,
+  added_at,
+  duration,
+  explicit,
+  album,
+}: Props) => {
+  const {
+    playbackState: { currentTrack },
+    togglePlayback,
+    setCurrentTrack,
+  } = usePlaybackQueryStore();
+  return (
+    <>
+      <HStack
+        justifyContent="space-between"
+        fontSize="sm"
+        _hover={{
+          bg: "gray.500",
+        }}
+        onClick={() => {
+          currentTrack != id ? setCurrentTrack(id) : togglePlayback();
+        }}
+        borderRadius={5}
+        paddingX={1}
+      >
+        {/* left section */}
+        <Box>
+          <HStack spacing={3}>
+            <Box boxSize="20px">
+              <Text size="2xl">{track_number}</Text>
+            </Box>
+            <VStack align="flex-start" spacing={1} paddingY={2}>
+              <Text as="b">
+                <Summary children={name} limit={35} />
+              </Text>
+
+              {/* artist names and explicit tags */}
+              <HStack spacing={1}>
+                {explicit && <ExplicitTag />}
+                {/* dynamically render artist names */}
+                <Show above="md">
+                  {artists.length < 3
+                    ? artists.map((artist, index) => (
+                        <Text key={index}>
+                          {index === artists.length - 1 ? artist : artist + ","}
+                        </Text>
+                      ))
+                    : artists
+                        .slice(0, 3)
+                        .map((artist, index) => (
+                          <Text>
+                            {index === 2 ? `${artist}...` : `${artist},`}
+                          </Text>
+                        ))}
+                </Show>
+              </HStack>
+            </VStack>
+          </HStack>
+        </Box>
+
+        {/* right section */}
+        <Show above="sm">
+          <HStack w="60%" justifyContent="space-between">
+            <Box
+              w="35%"
+              _hover={{
+                textDecoration: "underline",
+              }}
+            >
+              <Link to={album.link}>
+                <Summary children={album.name} limit={25} />
+              </Link>
+            </Box>
+            <Text>{added_at}</Text>
+            <Text>{duration}</Text>
+          </HStack>
+        </Show>
+      </HStack>
+    </>
+  );
+};
+
+export default PlaylistTrackItem;
--- src/components/playlist/PlaylistTracks.tsx
+++ src/components/playlist/PlaylistTracks.tsx
@@ -1,11 +1,10 @@
-import { Link, useParams } from "react-router-dom";
-import { Box, HStack, Show, Text, VStack } from "@chakra-ui/react";
-import ExplicitTag from "../ExplicitTag";
+import { useParams } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 import usePlaylist from "../../hooks/usePlaylist";
 import PlaylistTracksHeader from "./PlaylistTracksHeader";
 import TracklistSkeleton from "../album/TracklistSkeleton";
-import Summary from "../Summary";
 import useFormatQueryStore from "../../queries/format-store";
+import PlaylistTrackItem from "./PlaylistTrackItem";
 
 const PlaylistTracks = () => {
   const { id } = useParams();
@@ -29,67 +28,20 @@ const PlaylistTracks = () => {
       <Box paddingX={10}>
         <PlaylistTracksHeader />
         {playlist.tracks.items.map((t, index) => (
-          <HStack key={index} justifyContent="space-between" fontSize="sm">
-            {/* left section */}
-            <Box>
-              <HStack spacing={3}>
-                <Box boxSize="20px">
-                  <Text key={index} size="2xl">
-                    {index + 1}
-                  </Text>
-                </Box>
-                <VStack align="flex-start" spacing={1} paddingY={2}>
-                  <Text as="b">
-                    <Summary children={t.track.name} limit={35} />
-                  </Text>
-
-                  {/* artist names and explicit tags */}
-                  <HStack spacing={1}>
-                    {t.track.explicit && <ExplicitTag key={index} />}
-                    {/* dynamically render artist names */}
-                    <Show above="md">
-                      {t.track.artists.length < 3
-                        ? t.track.artists.map((artist, index) => (
-                            <Text key={index}>
-                              {index === t.track.artists.length - 1
-                                ? artist.name
-                                : artist.name + ","}
-                            </Text>
-                          ))
-                        : t.track.artists
-                            .slice(0, 3)
-                            .map((artist, index) => (
-                              <Text>
-                                {index === 2
-                                  ? `${artist.name}...`
-                                  : `${artist.name},`}
-                              </Text>
-                            ))}
-                    </Show>
-                  </HStack>
-                </VStack>
-              </HStack>
-            </Box>
-
-            {/* right section */}
-            <Show above="sm">
-              <HStack w="60%" justifyContent="space-between">
-                <Box
-                  key={index}
-                  w="35%"
-                  _hover={{
-                    textDecoration: "underline",
-                  }}
-                >
-                  <Link to={`/albums/${t.track.album.id}`} key={index}>
-                    <Summary children={t.track.album.name} limit={25} />
-                  </Link>
-                </Box>
-                <Text>{t.added_at.slice(0, 10)}</Text>
-                <Text>{formatDuration(t.track.duration_ms)}</Text>
-              </HStack>
-            </Show>
-          </HStack>
+          <PlaylistTrackItem
+            id={t.track.id}
+            key={index}
+            name={t.track.name}
+            track_number={index + 1}
+            artists={t.track.artists.map((a) => a.name)}
+            added_at={t.added_at.slice(0, 10)}
+            duration={formatDuration(t.track.duration_ms)}
+            explicit={t.track.explicit}
+            album={{
+              link: `/albums/${t.track.album.id}`,
+              name: t.track.album.name,
+            }}
+          />
         ))}
       </Box>
     </>
