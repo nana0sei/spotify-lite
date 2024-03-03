61518d92328e83ea82a3b6a5ffd06ad4811dcb24
nana0sei <frimpongnanao@gmail.com>
2024-03-03 11:55:27 +0000

    new releases cleanup

--- src/components/NewReleases.tsx
+++ src/components/NewReleases.tsx
@@ -1,8 +1,14 @@
-import { Card, Image, Text, VStack, SimpleGrid } from "@chakra-ui/react";
+import {
+  Image,
+  Text,
+  VStack,
+  SimpleGrid,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
 import useNewReleases from "../hooks/useNewReleases";
 import Summary from "./Summary";
 import { Link } from "react-router-dom";
-import AlbumCardSkeleton from "./album/AlbumCardSkeleton";
 
 const NewReleases = () => {
   const { data: newReleases, isLoading, error } = useNewReleases();
@@ -11,9 +17,9 @@ const NewReleases = () => {
   if (isLoading)
     return (
       <>
-        <SimpleGrid columns={{ sm: 2, md: 3, lg: 5 }} spacing={5} py={2}>
+        <SimpleGrid columns={{ sm: 2, md: 3, lg: 5 }} spacing={5} pt={2}>
           {skeletons.map((skeleton) => (
-            <AlbumCardSkeleton key={skeleton} />
+            <Skeleton key={skeleton} h="200px" w="200px" />
           ))}
         </SimpleGrid>
       </>
@@ -21,24 +27,22 @@ const NewReleases = () => {
   if (error || !newReleases) throw error;
   return (
     <>
-      <SimpleGrid columns={{ sm: 2, md: 3, lg: 5 }} spacing={5} py={2}>
+      <SimpleGrid columns={{ sm: 2, md: 3, lg: 5 }} spacing={5}>
         {newReleases.albums.items.map((album, index) => (
           <Link to={`/albums/${album.id}`} key={index}>
-            <Card key={index} bg="gray.800" _hover={{ bg: "gray.500" }}>
-              <VStack p={3} align="flex-start">
-                <Image src={album.images[0].url} key={index} />
-                <Text as="b">
-                  <Summary children={album.name} limit={13} />
-                </Text>
-                <Text key={index} fontSize="sm" color="gray.600">
-                  {album.artists.map((artist, index) =>
-                    index === album.artists.length - 1
-                      ? artist.name
-                      : artist.name + ", "
-                  )}
-                </Text>
-              </VStack>
-            </Card>
+            <VStack pt={2} align="flex-start">
+              <Image src={album.images[0].url} key={index} />
+              <Text as="b">
+                <Summary children={album.name} limit={20} />
+              </Text>
+              <Text key={index} fontSize="sm" color="gray.600">
+                {album.artists.map((artist, index) =>
+                  index === album.artists.length - 1
+                    ? artist.name
+                    : artist.name + ", "
+                )}
+              </Text>
+            </VStack>
           </Link>
         ))}
       </SimpleGrid>
