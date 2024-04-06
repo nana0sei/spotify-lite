a9f0794032e88985003f9db15fbba95b7a52c226
nana0sei <frimpongnanao@gmail.com>
2024-04-06 22:10:03 +0000

    added cards to new releases

--- src/components/NewReleases.tsx
+++ src/components/NewReleases.tsx
@@ -1,4 +1,11 @@
-import { Image, Text, VStack, SimpleGrid, Skeleton } from "@chakra-ui/react";
+import {
+  Image,
+  Text,
+  VStack,
+  SimpleGrid,
+  Skeleton,
+  Card,
+} from "@chakra-ui/react";
 import useNewReleases from "../hooks/useNewReleases";
 import Summary from "./Summary";
 import { Link } from "react-router-dom";
@@ -20,22 +27,24 @@ const NewReleases = () => {
   if (error || !newReleases) throw error;
   return (
     <>
-      <SimpleGrid columns={{ sm: 2, md: 3, lg: 5 }} spacing={5}>
+      <SimpleGrid columns={{ sm: 2, md: 3, lg: 5 }} spacing={5} pt={2}>
         {newReleases.albums.items.map((album, index) => (
           <Link to={`/albums/${album.id}`} key={index}>
-            <VStack pt={2} align="flex-start">
-              <Image src={album.images[0].url} key={index} />
-              <Text as="b">
-                <Summary children={album.name} limit={20} />
-              </Text>
-              <Text key={index} fontSize="sm" color="gray.600">
-                {album.artists.map((artist, index) =>
-                  index === album.artists.length - 1
-                    ? artist.name
-                    : artist.name + ", "
-                )}
-              </Text>
-            </VStack>
+            <Card bg="gray.800" _hover={{ bg: "gray.500" }}>
+              <VStack p={5} align="flex-start">
+                <Image src={album.images[0].url} />
+                <Text as="b">
+                  <Summary children={album.name} limit={20} />
+                </Text>
+                <Text fontSize="sm" color="gray.600">
+                  {album.artists.map((artist, index) =>
+                    index === album.artists.length - 1
+                      ? artist.name
+                      : artist.name + ", "
+                  )}
+                </Text>
+              </VStack>
+            </Card>
           </Link>
         ))}
       </SimpleGrid>
